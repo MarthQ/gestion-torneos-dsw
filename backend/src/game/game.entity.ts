85e06715125464dd19d4fa59951d590532974e39
MQRLZ <martinez34950@gmail.com>
2024-08-10 20:05:25 -0300

    Fixed issues in game and tournament entities.

--- backend/src/game/game.entity.ts
+++ backend/src/game/game.entity.ts
@@ -1,6 +1,16 @@
-import { Entity,PrimaryKey, Property, ManyToOne, ManyToMany, OneToMany, Cascade, Collection} from '@mikro-orm/core'
+import {
+    Entity,
+    PrimaryKey,
+    Property,
+    ManyToOne,
+    ManyToMany,
+    OneToMany,
+    Cascade,
+    Collection,
+    Rel,
+} from '@mikro-orm/core'
 import { BaseEntity } from '../shared/db/baseEntity.entity.js'
-import { Game_Type}  from '../game_type/game_type.entity.js'
+import { Game_Type } from '../game_type/game_type.entity.js'
 import { Tournament } from '../tournament/tournament.entity.js'
 import { Tag } from '../tag/tag.entity.js'
 
@@ -8,14 +18,16 @@ import { Tag } from '../tag/tag.entity.js'
 export class Game extends BaseEntity {
     @PrimaryKey()
     id?: number
-    @Property({nullable: false, unique:true})
+    @Property({ nullable: false, unique: true })
     name!: string
     @Property()
     cant_torneos!: number
-    @ManyToOne(() => Game_Type, {nullable: false})
-    gametype!: Game_Type
+    @ManyToOne(() => Game_Type, { nullable: false })
+    gametype!: Rel<Game_Type>
     @ManyToMany(() => Tag, (tag) => tag.games, { owner: true })
     tags!: Tag[]
-    @OneToMany(() => Tournament, (tournament) => tournament.game, {cascade: [Cascade.ALL]})
+    @OneToMany(() => Tournament, (tournament) => tournament.game, {
+        cascade: [Cascade.ALL],
+    })
     user = new Collection<Tournament>(this)
-} 
+}
