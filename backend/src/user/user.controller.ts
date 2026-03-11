3b290b7c4173dd893b10b0a388d80cf13bee1dce
mateoregodesebes <mateoregodesebes123@gmail.com>
2026-03-11 17:29:50 -0300

    Fixed zod implementation on user. userSchema wasn't being used

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -62,20 +62,31 @@ async function findOne(req: Request, res: Response) {
 
 async function add(req: Request, res: Response) {
     try {
-        const user = em.create(User, req.body)
-        await em.flush()
-        res.status(201).json({ message: 'User created', data: user })
+        const sanitizedUser = UserSchema.safeParse(req.body)
+
+        if (!sanitizedUser.success) {
+            throw fromZodError(sanitizedUser.error)
+        } else {
+            const user = em.create(User, sanitizedUser.data)
+            await em.flush()
+            res.status(201).json({ message: 'User created', data: user })
+        }
     } catch (error: any) {
         res.status(500).json({ message: error.message })
     }
 }
 async function update(req: Request, res: Response) {
     try {
-        const id = Number.parseInt(req.params.id)
-        const user = em.getReference(User, id)
-        em.assign(user, req.body)
-        await em.flush()
-        res.status(200).json({ message: 'User updated' })
+        const sanitizedPartialUser = UserSchema.partial().safeParse(req.body)
+        if (!sanitizedPartialUser.success) {
+            throw fromZodError(sanitizedPartialUser.error)
+        } else {
+            const id = Number.parseInt(req.params.id)
+            const user = em.getReference(User, id)
+            em.assign(user, req.body)
+            await em.flush()
+            res.status(200).json({ message: 'User updated' })
+        }
     } catch (error: any) {
         res.status(500).json(error.message)
     }
