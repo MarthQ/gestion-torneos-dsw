2e35ce2d6e705e1ba95f278cf9621a134c7b8655
JosepSoc <jose.socolsky@gmail.com>
2026-04-12 22:33:29 -0300

    Implemented integration test with Vitest

--- backend/src/shared/db/orm.ts
+++ backend/src/shared/db/orm.ts
@@ -2,15 +2,17 @@ import { MikroORM } from '@mikro-orm/mysql'
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter'
 import { env } from '../../config/env.js'
 
+const isTest = process.env.NODE_ENV === 'test'
+
 export const ORM = await MikroORM.init({
     entities: ['dist/**/*.entity.js'],
-    entitiesTs: ['src/**/*.entity.ts'],
+    entitiesTs: isTest ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
     dbName: env.DB_NAME,
     clientUrl: 'mysql://dsw:dsw@localhost:3306/okiDSW',
     highlighter: new SqlHighlighter(),
     debug: true,
+    allowGlobalContext: true,
     schemaGenerator: {
-        //Never in Production, only in dev
         disableForeignKeys: true,
         createForeignKeyConstraints: true,
         ignoreSchema: [],
